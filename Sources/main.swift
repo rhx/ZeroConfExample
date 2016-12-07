999ca3a6a943226147b466f04a88400abcaef08f
Rene Hexel <r.hexel@griffith.edu.au>
2016-12-07 12:20:20 +0000

    Squelch avahi compatibility warning

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,10 @@
 import CDNS_SD
 import Foundation
+#if os(Linux)
+  import Glibc
+
+  setenv("AVAHI_COMPAT_NOWARN", "1", 1)
+#endif
 
 var sdRef = DNSServiceRef(bitPattern: 0)
 let rv = DNSServiceRegister(&sdRef, 0, 0, "ComputerName", "_testtype._tcp", nil, nil, 1234, 0, nil, nil, nil)
