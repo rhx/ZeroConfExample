b0a1218e96d967d94bcec92100ce353ee3f8b7f9
Rene Hexel <r.hexel@griffith.edu.au>
2016-12-07 22:12:54 +1000

    Register a simple service for 20 seconds

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,11 @@
-print("Hello, world!")
+import CDNS_SD
+import Foundation
+
+var sdRef = DNSServiceRef(bitPattern: 0)
+let rv = DNSServiceRegister(&sdRef, 0, 0, "ComputerName", "_testtype._tcp", nil, nil, 1234, 0, nil, nil, nil)
+guard Int(rv) == kDNSServiceErr_NoError else {
+	print("Error \(rv) registering service")
+	exit(EXIT_FAILURE)
+}
+
+RunLoop.current.run(until: Date(timeIntervalSinceNow: 20))
