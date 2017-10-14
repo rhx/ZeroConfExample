99abd475812808281f5f3b25f2947737c5031944
Rene Hexel <r.hexel@griffith.edu.au>
2017-10-15 08:27:55 +1000

    Use CDNS_Services under macOS

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
 import PackageDescription
 
+#if os(macOS)
+    let cdns_sd = "CDNS_Services"
+#else
+    let cdns_sd = "CDNS_SD"
+#endif
+
 let package = Package(
     name: "ZeroConfExample",
     dependencies: [
-        .Package(url: "https://github.com/rhx/CDNS_SD.git", majorVersion: 1),
-    ]
+        .Package(url: "https://github.com/rhx/\(cdns_sd).git", majorVersion: 1),
+    ],
+    swiftLanguageVersions: [3, 4]
 )
-
